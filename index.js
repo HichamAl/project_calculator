90160bd9a99877efa63e337b0d8f06db7059f791
Beghdad Almakroudi <beghdadalmakroudi@MacBook-Pro-van-Beghdad.local>
2025-02-24 23:51:50 +0100

    ADDED html Calculator with css, and calculator logic in js file

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("tets");
+console.log("test");
+
+const number1 = null;
+const operator = null;
+const number2 = null;
+
+function operate(number1, operator, number2){
+    if (operator === "+"){
+        addition(number1, number2);
+
+    } else if (operator === "-"){
+        subtraction(number1, number2);
+
+    } else if (operator === "*"){
+        multiplication(number1, number2);
+        
+    } else if (operator === "/"){
+        division(number1, number2);
+        
+    }
+
+}
+
+function addition(number1, number2){
+    result = number1 + number2;
+    return result
+}
+
+function subtraction(number1, number2){
+    result = number1 - number2;
+    return result
+}
+
+function multiplication(number1, number2){
+    result = number1 * number2;
+    return result
+}
+
+function division(number1, number2){
+    result = number1 / number2;
+    return result
+}
