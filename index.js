f71c785768e69087c27c81dc9eb20b99d5152a79
Beghdad Almakroudi <beghdadalmakroudi@MacBook-Pro-van-Beghdad.local>
2025-03-09 17:58:55 +0100

    make it work with hardcoded values, now only values need to be gotten from input

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-let number1 = null;
-let operator = null;
-let number2 = null;
+// use these to update display?
+let number1 = 10;
+let operator = "*";
+let number2 = 20;
 
-function populateDisplay(number1, operator, number2){
+const displayScreen = document.querySelector(".white-screen");
+let operationText = document.createTextNode(number1 + operator + number2); 
+displayScreen.appendChild(operationText);
 
+// get values from user input
+// store the value in variables above
+
+if (operator === "*"){
+    console.log(multiplication(number1,number2));
+} else if (operator === "+") {
+    console.log(addition(number1,number2));
+} else if (operator === "-"){
+    console.log(subtraction(number1,number2));
+} else if (operator === "/"){
+    console.log(division(number1,number2));
 }
 
-function operate(number1, operator, number2){
-    if (operator === "+"){
-        addition(number1, number2);
-
-    } else if (operator === "-"){
-        subtraction(number1, number2);
-
-    } else if (operator === "*"){
-        multiplication(number1, number2);
-        
-    } else if (operator === "/"){
-        division(number1, number2);
-        
-    }
-
-}
-
+// already tested successfully
 function addition(number1, number2){
     result = number1 + number2;
     return result
