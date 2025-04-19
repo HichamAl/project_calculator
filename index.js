69c43abdf50089da149226950395455db5819d3c
HichamAl <104017860+HichamAl@users.noreply.github.com>
2025-04-19 13:03:47 +0200

    remove clutter, improve naming, added console.log for every button clicked using data action in html and event delegation

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-// use these to update display?
-let number1 = "";
-let operator = "";
-let number2 = "";
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculatorKeys");
 
-const displayScreen = document.querySelector(".white-screen");
-const numbers = document.querySelectorAll(".numbers button");
-const numberOne = document.createElement("p");
-const operatorDisplayed = document.createElement("p");
-const numberTwo = document.createElement("p");
-displayScreen.appendChild(numberOne);
-displayScreen.appendChild(operatorDisplayed);
-displayScreen.appendChild(numberTwo);
+keys.addEventListener('click', function (e){
+    if (e.target.matches('button')){ 
+        const key = e.target;
+        const action = key.dataset.action;
 
-numbers.forEach((numbers) => {
-    // and for each one we add a 'click' listener
-    numbers.addEventListener("click", () => {
-        if (operator.length === 1){
-            number2 += numbers.innerHTML;
-        } else {
-            number1 += numbers.innerHTML;
-        }   
-        numberOne.innerHTML = number1;
-        numberTwo.innerHTML = number2;
-    });
-  });
+        if (!action){
+            console.log('number key');
+        }
 
-const operators = document.querySelectorAll(".operators button");
-operators.forEach((operators) => {
-    // and for each one we add a 'click' listener
-    operators.addEventListener("click", () => {
-      operator += operators.innerHTML;
-      operatorDisplayed.innerHTML = operator.slice(-1); // get last operator clicked
-    });
-  });
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide' 
+        ) {
+            console.log('operator key');
+        }
+
+        if (action === 'decimal'){
+            console.log('decimal key');
+        }
+
+        if (action === 'clear'){
+            console.log('clear key');
+        }
+
+        if (action === 'calculate'){
+            console.log('calculate key');
+        }
+        
+    }
+
+    
+
+})
+
+
+
 
 // already tested successfully
 function addition(number1, number2){
