b45d1b100a7fe636253c0344a067dd3a37804f9d
HichamAl <104017860+HichamAl@users.noreply.github.com>
2025-06-15 00:50:02 +0200

    Het opslaan van het eerste nummer in variabele number1 , zodat gebruiker een eerste nummer voor calculatie kan maken, voorlopig via console.log alleen

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-let number1 = "10";
-let number2 = "5";
-let operator = "-";
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+      const value = button.innerHTML;
+      console.log(typeof(value));
+      console.log(value);
+
+      // eerst happy path dat alles perfect gaat eerst 2 cijfers dan
+      // operator dan weer 2 cijfers 
+
+      console.log(isNaN(value));
+      if (isNaN(value) == false){
+        number1 += value;
+
+        console.log(number1);
+      }
+      
+
+      // THis project is difficult 
 
 
+      // current problem = when appending the number to the display 
+      // old clicked number gets appended the new number plus old number
+      // example: user clicks 1 (which gets appended to screen is good)
+      // then user clicks 1 again (the screen now has three 1's) 111
+      // the first one is the old one and the second and third is the 
+      // new number 11 which is good
+    });
+  });
+
+
+// this function makes the calculator work unlimited amount of times
 function operate (number1, operator, number2){
-    if (operator == "+"){
-        console.log(add(number1, number2));
-    }
-    if (operator == "-"){
-        console.log(subtract(number1, number2));
-    }
-    if (operator == "*"){
-        console.log(multiplication(number1, number2));
-    }
-    if (operator == "/"){
-        console.log(divide(number1, number2));
-    }
+    
 }
 
 
